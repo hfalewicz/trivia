de8776e26fa4c2f54dde4ffd7cc686e972d7adcc
hfalewicz <programmer@hs-lab-23.local>
2015-10-21 10:09:57 -0400

    Start Code

---
+++ trivia.py
@@ -0,0 +1,45 @@
+#regular functions
+def ask_question(): 
+    """Print out a question"""
+    print (‘What is Mickey’s pet dogs name?’)
+    #write code
+
+def multiple_choice(): 
+    """Print out multiple choice options"""
+   print(‘a. Donald’, ‘b. Pluto’, ‘c. Goofy’, ‘d. Daisy’)
+    #write code
+
+def answer(): 
+    """Correct answer for each question"""
+    correct_1= b
+    #write code
+
+def user_answer():
+    """Figure out is user’s answer is right or wrong"""
+    #write code
+
+def play_game(): 
+   for question,choices,correct in [[‘What is Mickey’s pet dogs’ name?’, [‘Donald’, ‘Pluto’, ‘Goofy’, ‘Daisy’], ‘b’], [‘What animal swallowed a clock in Peter Pan?’, [‘Tiger’, ‘Parrot’, ‘Crocodile’, ‘Shark’], ‘c’], #etc.]: 
+        print(question)
+        print(choices)
+        user_answer= raw_input(‘What is your answer?’) 
+            if user_answer= correct
+                print ‘You got the problem correct!’
+            if user_answer=!= correct
+                print ‘The correct answer was’, correct
+    print ‘You have completed all questions. Thank you for playing.’
+
+
+#test functions
+def test_ask_question(): 
+    print question
+
+def test_multiple_choice():    
+    print multiple_choice
+
+def test_answer(): 
+    print question_and_answer
+
+def test_user_answer(): 
+    print question_and_
+    answer= raw_input(‘What is your answer?’) 
