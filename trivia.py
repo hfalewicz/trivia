3ecaf8c8846782c8f967eef8a8c9e0a6b5112de4
hfalewicz <programmer@hs-lab-23.local>
2015-10-26 13:07:22 -0400

    Changes

--- trivia.py
+++ trivia.py
@@ -1,7 +1,7 @@
 #regular functions
 def ask_question(): 
     """Print out a question"""
-    print ("What is Mickey’s pet dogs name?")
+    print ("What is Mickey's pet dogs name?")
     print ("What animal shallowed a clock in Peter Pan?")
     print ("Who is the first Disney princess?")
     print ("What U.S. city is The Princess and The Frog set in?")
@@ -13,16 +13,16 @@ def ask_question():
 
 def multiple_choice(): 
     """Print out multiple choice options"""
-   print("a. Donald", "b. Pluto", "c. Goofy", "d. Daisy")
-   print("a. Tiger", "b. Parrot", "c. Crocodile", "d. Shark")
-   print("a. Snow White", "b. Pocahontas", "c. Tiana", "d. Cinderella")
-   print("a. Nashville", "b. New York", "c. Atlanta", "d. New Orleans")
-   print("a. Love Rock", "b. Pride Rock", "c. Justice Rock", "d. Peace Rock")
-   print("a. Gold", "b. Silver", "c. Bronze", "d. None of the above")
-   print("a. Dog", "b. Pluto", "c. Max", "d. Mike")
-   print("a. First star to the right and straight on  til morning", "b. Second star to the left and straight on til morning", "c. Third star to the right and straight on til morning","d. Second star to the right and straight on til morning")
-   print("a. Jesse and Goose", "b. Jess and Jack-Jack", "c. Gus and Jaq", "d. Wes and Jess")
-   print("a. Jim A Sullivan", "b. Sullivan", "c. J P Sullivan", "d. James P Sullivan")
+    print("a. Donald", "b. Pluto", "c. Goofy", "d. Daisy")
+    print("a. Tiger", "b. Parrot", "c. Crocodile", "d. Shark")
+    print("a. Snow White", "b. Pocahontas", "c. Tiana", "d. Cinderella")
+    print("a. Nashville", "b. New York", "c. Atlanta", "d. New Orleans")
+    print("a. Love Rock", "b. Pride Rock", "c. Justice Rock", "d. Peace Rock")
+    print("a. Gold", "b. Silver", "c. Bronze", "d. None of the above")
+    print("a. Dog", "b. Pluto", "c. Max", "d. Mike")
+    print("a. First star to the right and straight on  til morning", "b. Second star to the left and straight on til morning", "c. Third star to the right and straight on til morning","d. Second star to the right and straight on til morning")
+    print("a. Jesse and Goose", "b. Jess and Jack-Jack", "c. Gus and Jaq", "d. Wes and Jess")
+    print("a. Jim A Sullivan", "b. Sullivan", "c. J P Sullivan", "d. James P Sullivan")
 
 def answer(): 
     """Correct answer for each question"""
@@ -38,14 +38,14 @@ def answer():
     correct_10= "d" 
 
 def user_answer():
-    """Figure out is user’s answer is right or wrong"""
+    """Figure out is user's answer is right or wrong"""
     raw_input("What is your answer?") 
 
 def play_game(): 
    for question,choices,correct in [
-           ["What is Mickey’s pet dogs’ name?", ["a. Donald", "b. Pluto", "c. Goofy", "d. Daisy"], "b"],
+           ["What is Mickey's pet dogs' name?", ["a. Donald", "b. Pluto", "c. Goofy", "d. Daisy"], "b"],
            ["What animal swallowed a clock in Peter Pan?", ["a. Tiger", "b. Parrot", "c. Crocodile", "d.  Shark"], "c"],
-           ["What is Mickey’s pet dog's name?", ["a. Donald", "b. Pluto", "c. Goofy", "d. Daisy"], "b"],
+           ["What is Mickey's pet dog's name?", ["a. Donald", "b. Pluto", "c. Goofy", "d. Daisy"], "b"],
            ["What animal swallowed a clock in Peter Pan?", ["a. Tiger", "b. Parrot", "c. Crocodile", "d. Shark"], "c"],
            ["Who is the first Disney princess?", ["a. Snow White","b. Pocahontas", "c. Tiana"," d. Cinderella"], "a"],
            ["What U.S. city is The Princess and The Frog set in?", ["a. Nashville", "b. New York", "c. Altana", "d. New Orleans"], "d"],
@@ -58,12 +58,12 @@ def play_game():
    ]: 
         print(question)
         print(choices)
-        user_answer= raw_input("What is your answer?") 
-            if user_answer= correct
-                print ("You got the problem correct!")
-            if user_answer=!= correct
-                print ("The correct answer was", correct)
-    print ("You have completed all questions. Thank you for playing.")
+        user_answer= raw_input("What is your answer?")
+        if user_answer==correct:
+            print ("You got the problem correct!")
+        else:
+            print ("The correct answer was", correct)
+        print ("You have completed all questions. Thank you for playing.")
 
 
 #test functions
