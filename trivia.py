2864765bb204e4bcafd500a8358fa660184d5491
hfalewicz <programmer@hs-lab-23.local>
2015-10-26 12:16:55 -0400

    Changes

--- trivia.py
+++ trivia.py
@@ -36,20 +36,21 @@ def answer():
 def user_answer():
     """Figure out is user’s answer is right or wrong"""
     raw_input("What is your answer?") 
-    #write code
 
 def play_game(): 
    for question,choices,correct in [
            ["What is Mickey’s pet dogs’ name?", ["a. Donald", "b. Pluto", "c. Goofy", "d. Daisy"], "b"],
            ["What animal swallowed a clock in Peter Pan?", ["a. Tiger", "b. Parrot", "c. Crocodile", "d.  Shark"], "c"],
+           ["What is Mickey’s pet dog's name?", ["a. Donald", "b. Pluto", "c. Goofy", "d. Daisy"], "b"],
+           ["What animal swallowed a clock in Peter Pan?", ["a. Tiger", "b. Parrot", "c. Crocodile", "d. Shark"], "c"],
            ["Who is the first Disney princess?", ["a. Snow White","b. Pocahontas", "c. Tiana"," d. Cinderella"], "a"],
            ["What U.S. city is The Princess and The Frog set in?", ["a. Nashville", "b. New York", "c. Altana", "d. New Orleans"], "d"],
            ["In the Lion King, where does Mufasa and his family live?", ["a. Love Rock","b. Pride Rock", "c. Justice Rock", "d. Peace Rock"], "b"],
-           ["During the ballroom scene of Beauty & the Beast, what color is her dress?", ["a. Gold", "b. Silver", "c. Bronze", "d. None of the above"], "a"],
-           ["In the Little Mermaid, what is the name of the dog?", ["a. Dog", "b. Pluto", "c. Max", "d. Mike"], "c"],
+           ["During the ballroom scene of Beauty & the Beast, what color is Belle's  dress?", ["a. Gold", "b. Silver", "c. Bronze", "d. None of the above"], "a"],
+           ["In the Little Mermaid, what is Prince Eric's dog's name?", ["a. Dog", "b. Pluto", "c. Max", "d. Mike"], "c"],
            ["In Peter Pan, what is the way to Neverland?", ["a. First star to the right and straight on  til morning", "b. Second star to the left and straight on til morning", "c. Third star to the right and straight on til morning","d. Second star to the right and straight on til morning"], "d"],
-           ["What is the name of the mice in Cinderella?", ["a. Jesse and Goose", "b. Jess and Jack-Jack", "c. Gus and Jaq", "d. Wes and Jess"], "c"],
-           ["In Monsters Inc., what is the full name for Sulley?", ["a. Jim A Sullivan", "b. Sullivan", "c. J P Sullivan", "d. James P Sullivan"], "d"]
+           ["What is the name of Cinderella's mice?", ["a. Jesse and Goose", "b. Jess and Jack-Jack", "c. Gus and Jaq", "d. Wes and Jess"], "c"],
+           ["In Monsters Inc., what is Sulley's full name?", ["a. Jim A Sullivan", "b. Sullivan", "c. J P Sullivan", "d. James P Sullivan"], "d"]
    ]: 
         print(question)
         print(choices)
@@ -63,14 +64,14 @@ def play_game():
 
 #test functions
 def test_ask_question(): 
-    print question
+    print (question)
 
 def test_multiple_choice():    
-    print multiple_choice
+    print (multiple_choice)
 
 def test_answer(): 
-    print question_and_answer
+    print (question_and_answer)
 
 def test_user_answer(): 
-    print question_and_
-    answer= raw_input(‘What is your answer?’) 
+    print (question_and_)
+    answer= raw_input("What is your answer?") 
