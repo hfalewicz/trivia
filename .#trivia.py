43e0d49d7bec806214bc45af677c3b1276441f89
hfalewicz <programmer@hs-lab-23.local>
2015-11-02 12:59:45 -0500

    Changes

--- .#trivia.py
+++
@@ -1,1 +0,0 @@
-programmer@hs-lab-23.local.17834
--- trivia.py
+++ trivia.py
@@ -1,35 +1,14 @@
 #regular functions
 #Ruby and Hannah
 def ask_question(question): 
->>>>>>> origin/master
     """Print out a question"""
     print question
-    # print ("What is Mickey's pet dogs name?")
-    # print ("What animal shallowed a clock in Peter Pan?")
-    # print ("Who is the first Disney princess?")
-    # print ("What U.S. city is The Princess and The Frog set in?")
-    # print ("In The Lion King, where does Mufasa and his family live?")
-    # print ("In the Little Mermaid, what is the name of Prince Eric's dog?")
-    # print ("In Peter pan, what is the way to Neverland?")
-    # print ("What is the name of Cinderella's mice?")
-    # print ("In Monster's Inc, what is Sulley's full name?")
 
 #Ruby and Hannah
 def multiple_choice(choices): 
->>>>>>> origin/master
     """Print out multiple choice options"""
     print choices
-    return raw_input('what is your choice:') 
-    print("a. Donald", "b. Pluto", "c. Goofy", "d. Daisy")
-    print("a. Tiger", "b. Parrot", "c. Crocodile", "d. Shark")
-    print("a. Snow White", "b. Pocahontas", "c. Tiana", "d. Cinderella")
-    print("a. Nashville", "b. New York", "c. Atlanta", "d. New Orleans")
-    print("a. Love Rock", "b. Pride Rock", "c. Justice Rock", "d. Peace Rock")
-    print("a. Gold", "b. Silver", "c. Bronze", "d. None of the above")
-    print("a. Dog", "b. Pluto", "c. Max", "d. Mike")
-    print("a. First star to the right and straight on  til morning", "b. Second star to the left and straight on til morning", "c. Third star to the right and straight on til morning","d. Second star to the right and straight on til morning")
-    print("a. Jesse and Goose", "b. Jess and Jack-Jack", "c. Gus and Jaq", "d. Wes and Jess")
-    print("a. Jim A Sullivan", "b. Sullivan", "c. J P Sullivan", "d. James P Sullivan")
+    return raw_input('What is your choice?') 
 
 #Ruby and Hannah
 def answer(): 
@@ -46,9 +25,9 @@ def answer():
     correct_10= "d" 
 
 #Ruby and Hannah
-def user_answer():
-    """Figure out is user's answer is right or wrong"""
-    raw_input("What is your answer?") 
+#def user_answer():
+    #"""Figure out is user's answer is right or wrong"""
+    #raw_input("What is your answer?") 
 
 #Hannah
 def play_game(): 
